82ae9ca444064416a0106f3f5d4f3504fdb5a0c1
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:21:55 +0530

    BitQueue

---
+++ bitqueueex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace bitqueueex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue my_queue = new Queue();
+            my_queue.Enqueue("Sujit");
+            my_queue.Enqueue(2);
+            my_queue.Enqueue(200);
+            my_queue.Enqueue(null);
+            my_queue.Enqueue(4.4);
+            my_queue.Enqueue("Pankaj");
+            foreach (var ele in my_queue)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
